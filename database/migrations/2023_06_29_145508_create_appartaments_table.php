ec9447c5be020e99dbb31d9ae3606835776ee055
StefanoM98 <smartino030@gmail.com>
2023-06-29 17:08:04 +0200

    Add: Created Appartment Table

---
+++ database/migrations/2023_06_29_145508_create_appartaments_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appartaments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('name', 100);
+            $table->string('slug')->unique();
+            $table->text('description');
+            $table->unsignedSmallInteger('square_meters');
+            $table->unsignedTinyInteger('bed_number');
+            $table->unsignedTinyInteger('bathroom_number');
+            $table->unsignedTinyInteger('room_number');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('latitude', 100);
+            $table->string('longitude', 100);
+            $table->float('price', 6, 2)->unsigned();
+            $table->string('image', 255)->nullable();
+            $table->boolean('visibility')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appartaments');
+    }
+};
