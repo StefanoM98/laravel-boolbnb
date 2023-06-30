d44a926b3ed9d8b8445927166876124b29c09507
diegoG82 <diegogiardina@hotmail.it>
2023-06-30 10:55:22 +0200

    created model apartmentsponsor

---
+++ app/Models/ApartmentSponsor.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ApartmentSponsor extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'apartment_id',
+        'sponsor_id'
+    ];
+
+    protected $table = 'apartment_sponsor';
+}
