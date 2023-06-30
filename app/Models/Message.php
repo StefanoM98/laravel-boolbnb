d8497b33e705a0da30c329f438b25b60f9727c7a
diegoG82 <diegogiardina@hotmail.it>
2023-06-30 10:45:14 +0200

    created message model

---
+++ app/Models/Message.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'text',
+        'state_message'
+    ];
+
+    // creata relazione con la tabella apartments
+    public function apartments() {
+        return $this->belongsTo(Apartment::class);
+    }
+}
