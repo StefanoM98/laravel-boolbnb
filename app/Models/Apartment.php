d9b8360ba1cd2097e705a6aa77f2b05e878940a7
diegoG82 <diegogiardina@hotmail.it>
2023-06-30 10:26:14 +0200

    created apartment & service model

---
+++ app/Models/Apartment.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Apartment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'slug',
+        'description',
+        'square_meters',
+        'bed_number',
+        'bathroom_number',
+        'room_number',
+        'address',
+        'city',
+        'state',
+        'latitude',
+        'longitude',
+        'price',
+        'image',
+        'visibility'
+    ];
+
+    // Creata relazione con tabella user
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+    // Creata relazione con tabella service
+    public function service() {
+        return $this->belongsToMany(Service::class, 'apartment_service');
+    }
+
+}
+
