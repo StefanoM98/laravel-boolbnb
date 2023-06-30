a42548f0cbec0d3bfdf77ad01438894b86dbb283
diegoG82 <diegogiardina@hotmail.it>
2023-06-30 10:30:13 +0200

    created sponsor model

---
+++ app/Models/Sponsor.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sponsor extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'price',
+        'duration',
+        'description'
+    ];
+    // creata relazione con la tabella apartments
+    public function apartments() {
+        return $this->belongsToMany(Apartment::class);
+    }
+}
