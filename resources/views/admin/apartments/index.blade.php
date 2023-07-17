4eb50fc3a9efc349cbcc12864a5775b0945950c4
LucaPertosa <luca.pertosa@hotmail.it>
2023-07-17 17:22:48 +0200

    added viewport breakpoint

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.admin')
 
 @section('content')
-    @include('partials.session_message')
     <div class="container">
+        @include('partials.session_message')
         <h1 class=" my-3  p_color"> My Apartments list</h1>
 
         <div class="my-4">
@@ -12,35 +12,37 @@
             <table class="table align-middle">
                 <thead class="text-danger">
                     <tr>
-                        <th scope="col ">Name</th>
-                        <th scope="col">City</th>
-                        <th scope="col">Address</th>
+                        <th scope="col">Name</th>
+                        <th scope="col" class="d-none d-lg-table-cell">City</th>
+                        <th scope="col" class="d-none d-lg-table-cell">Address</th>
                         <th scope="col">Price</th>
-                        <th scope="col">Actions</th>
+                        <th scope="col" class="text-center">Actions</th>
                     </tr>
                 </thead>
-    
+
                 <tbody>
                     @foreach ($apartments as $apartment)
                         <tr class="text-capitalize">
                             <td>{{ $apartment->name }}</td>
-                            <td>{{ $apartment->city }}</td>
-                            <td>{{ $apartment->address }}</td>
+                            <td class="d-none d-lg-table-cell">{{ $apartment->city }}</td>
+                            <td class="d-none d-lg-table-cell">{{ $apartment->address }}</td>
                             <td>{{ $apartment->price }}€</td>
-                            <td class="d-flex justify-center align-items-center">
-                                <div>
-                                    <a href="{{ route('admin.apartments.show', $apartment->slug) }}" class="btn btn-success">
+                            <td class="">
+                                <div class="d-flex justify-content-between align-items-end">
+                                    <a href="{{ route('admin.apartments.show', $apartment->slug) }}"
+                                        class="btn btn-success">
                                         <i class="fa-solid fa-eye"></i>
                                     </a>
-                                    <a class="btn btn-warning" href="{{ route('admin.apartments.edit', $apartment->slug) }}">
+                                    <a class="btn btn-warning"
+                                        href="{{ route('admin.apartments.edit', $apartment->slug) }}">
                                         <i class="fa-regular fa-pen-to-square"></i>
                                     </a>
-    
-                                    <form class="d-inline-block"
+
+                                    <form class="d-inline-block m-0"
                                         action="{{ route('admin.apartments.destroy', $apartment->slug) }}" method="POST">
                                         @csrf
                                         @method('DELETE')
-    
+
                                         <button type="submit" class="btn btn-danger ms_btn_cancel"
                                             data-title="{{ $apartment->name }}">
                                             <i class="fa-solid fa-trash"></i>
@@ -61,7 +63,7 @@
         @endif
 
     </div>
-   
+
     {{-- <div>
         {{ $projects->links() }}
     </div> --}}
@@ -74,23 +76,22 @@
     </script> --}}
 
     <style lang="scss" scoped>
-
-        .btn_n{
+        .btn_n {
             background: #24ADE3;
             color: white;
 
         }
-        .btn:hover{
+
+        .btn:hover {
             background: #1e92bf
         }
 
         :root {
-        --primary-color: #24ADE3
-    }
+            --primary-color: #24ADE3
+        }
 
-    .p_color {
-        color: var(--primary-color)
-    }
-
+        .p_color {
+            color: var(--primary-color)
+        }
     </style>
 @endsection
